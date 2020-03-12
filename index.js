761585ef895af7137b4c29d94d0a70201a4cb7bf
getmanovanton <getmanov11@mail.ru>
2020-03-12 21:30:56 +0200

    first

---
+++ index.js
@@ -0,0 +1,120 @@
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
+    //////////////////////////////////////////    PRE-LOADER
+
+    let loader = document.querySelector('.preload');
+    function loaderOff() {
+        loader.style.opacity = "0";
+        loader.style.visibility = "hidden";
+    }
+    setTimeout(loaderOff, 3000);
+
+
+    //////////////////////////////////////////    MAIN
+
+    let button = document.querySelector('.todo-submit'),
+        toDoText = document.querySelector('.todo-text'),
+        hint = document.querySelector('.hint'),
+        massageArea = document.querySelector('.todo-massages'),
+        list = [];
+
+
+    function saveMassage(massageText){
+        if(massageText === "" || massageText[0] === " "){
+            hint.style.opacity = "1"
+        } else {
+            hint.style.opacity = "0";
+            let div = document.createElement('div'),
+                btn = document.createElement('div'),
+                str = document.createElement("p"),
+                temp = {},
+                i = list.length;
+            btn.classList.add('delete-massage');
+            div.classList.add('massage');
+            str.classList.add('massage-text');
+            str.textContent = massageText;
+            massageArea.appendChild(div);
+            div.appendChild(str);
+            div.appendChild(btn);
+            temp.todo = massageText;
+            temp.check = false;
+            list[i] = temp;
+            toDoText.value = "";
+            localStorage.setItem("todo", JSON.stringify(list));
+
+    }
+    }
+
+/////////////////////////////////////////    DELETE DONE MASSAGE
+
+    massageArea.addEventListener('click', function (e) {
+        let event = e.target;
+        if(event.classList.contains("massage")){
+            event.classList.toggle("done");
+            list.forEach((txt, index) => {
+                if(txt.todo === event.textContent && list[index].check !== true){
+                        list[index].check = true;
+                        localStorage.setItem("todo", JSON.stringify(list))
+                } else if (txt.todo === event.textContent && list[index].check === true){
+                    list[index].check = false;
+                    localStorage.setItem("todo", JSON.stringify(list))
+                }
+            });
+        }
+        else if (event.classList.contains("massage-text")){
+            event.closest(".massage").classList.toggle("done");
+            list.forEach((txt, index) => {
+                if(txt.todo === event.textContent && list[index].check !== true){
+                    list[index].check = true;
+                    localStorage.setItem("todo", JSON.stringify(list))
+                }else if (txt.todo === event.textContent && list[index].check === true){
+                    list[index].check = false;
+                    localStorage.setItem("todo", JSON.stringify(list))
+                }
+            });
+        }
+        else if (event.classList.contains("delete-massage")){
+            list.forEach((txt, index) => {
+                if(txt.todo === event.closest(".massage").textContent){
+                    list.splice(index ,1);
+                    event.closest(".massage").remove();
+                    localStorage.setItem("todo", JSON.stringify(list))
+                }
+            });
+
+        }
+    })
+    /////////////////////////////////////////    SAVE MASSAGE ONCLICK
+    button.addEventListener('click', ()=> {saveMassage(toDoText.value)} );
+
+    document.onkeyup = function (e) {
+        if(e.code === "Enter"){
+            saveMassage(toDoText.value)
+        }
+    }
+
+    /////////////////////////////////////////    LOCAL STORAGE
+    
+    if(localStorage.getItem('todo') !== null){
+        list = JSON.parse(localStorage.getItem('todo')) ;
+        list.forEach(txt =>{
+            let div = document.createElement('div'),
+                btn = document.createElement('div'),
+                str = document.createElement("p");
+            btn.classList.add('delete-massage');
+            div.classList.add('massage');
+            str.classList.add('massage-text');
+            str.textContent = txt.todo;
+            massageArea.appendChild(div);
+            div.appendChild(str);
+            div.appendChild(btn);
+            if (txt.check === true){
+                div.classList.toggle('done');
+            }
+        })
+    }
+
+
+
+});
