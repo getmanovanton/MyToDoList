ab164dc0588b65438dcc35081a5737d6b9a99f60
getmanovanton <getmanov11@mail.ru>
2020-03-13 14:37:11 +0200

    finifh

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', function () {
         loader.style.opacity = "0";
         loader.style.visibility = "hidden";
     }
-    setTimeout(loaderOff, 3000);
+    setTimeout(loaderOff, 0);
 
 
 
@@ -17,14 +17,23 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
 
-
-     let date = new Date(),
-         dateArea = document.querySelector(".date"),
-         day = date.getDate(),
-         month = date.toLocaleString('en', { month: 'long' }),
-         year = date.getFullYear();
+    function todayDate(){
+        let date = new Date(),
+            dateArea = document.querySelector(".date"),
+            day = date.getDate(),
+            month = date.toLocaleString('en', { month: 'long' }),
+            year = date.getFullYear(),
+            hours = date.getHours(),
+            minutes = date.getMinutes();
+        if (minutes<10){
+            minutes = "0" + minutes
+        }
+        if (hours<10){
+            hours = "0" + hours
+        }
         dateArea.innerHTML = ` Today: ${day} ${month} ${year}`;
-
+    }
+    todayDate();
 
 
     //////////////////////////////////////////    MAIN
@@ -42,20 +51,41 @@ window.addEventListener('DOMContentLoaded', function () {
             hint.style.opacity = "1"
         } else {
             hint.style.opacity = "0";
+
             let div = document.createElement('div'),
                 btn = document.createElement('div'),
+                time = document.createElement('div'),
                 str = document.createElement("p"),
+                date = new Date(),
+                day = date.getDate(),
+                month = date.toLocaleString('en', { month: 'long' }),
+                hours = date.getHours(),
+                minutes = date.getMinutes(),
                 temp = {},
                 i = list.length;
+
+
+            if (minutes<10){
+                minutes = "0" + minutes
+            }
+            if (hours<10){
+                hours = "0" + hours
+            }
+
+
             btn.classList.add('delete-massage');
             div.classList.add('massage');
+            time.classList.add('massage-time');
             str.classList.add('massage-text');
             str.textContent = massageText;
+            time.innerHTML = `${day} ${month} ${hours}:${minutes}`;
             massageArea.appendChild(div);
             div.appendChild(str);
             div.appendChild(btn);
+            div.appendChild(time);
             temp.todo = massageText;
             temp.check = false;
+            temp.time = time.textContent;
             list[i] = temp;
             toDoText.value = "";
             localStorage.setItem("todo", JSON.stringify(list));
@@ -95,10 +125,10 @@ window.addEventListener('DOMContentLoaded', function () {
         if(event.classList.contains("massage")){
             event.classList.toggle("done");
             list.forEach((txt, index) => {
-                if(txt.todo === event.textContent && list[index].check !== true){
+                if(txt.todo + txt.time === event.textContent && list[index].check !== true){
                         list[index].check = true;
                         localStorage.setItem("todo", JSON.stringify(list))
-                } else if (txt.todo === event.textContent && list[index].check === true){
+                } else if (txt.todo + txt.time === event.textContent && list[index].check === true){
                     list[index].check = false;
                     localStorage.setItem("todo", JSON.stringify(list))
                 }
@@ -107,10 +137,10 @@ window.addEventListener('DOMContentLoaded', function () {
         else if (event.classList.contains("massage-text")){
             event.closest(".massage").classList.toggle("done");
             list.forEach((txt, index) => {
-                if(txt.todo === event.textContent && list[index].check !== true){
+                if(txt.todo + txt.time === event.textContent && list[index].check !== true){
                     list[index].check = true;
                     localStorage.setItem("todo", JSON.stringify(list))
-                }else if (txt.todo === event.textContent && list[index].check === true){
+                }else if (txt.todo + txt.time === event.textContent && list[index].check === true){
                     list[index].check = false;
                     localStorage.setItem("todo", JSON.stringify(list))
                 }
@@ -118,13 +148,25 @@ window.addEventListener('DOMContentLoaded', function () {
         }
         else if (event.classList.contains("delete-massage")){
             list.forEach((txt, index) => {
-                if(txt.todo === event.closest(".massage").textContent){
+                if(txt.todo + txt.time === event.closest(".massage").textContent )  {
                     list.splice(index ,1);
                     event.closest(".massage").remove();
                     localStorage.setItem("todo", JSON.stringify(list))
                 }
             });
 
+        }
+        else if (event.classList.contains("massage-time")){
+            event.closest(".massage").classList.toggle("done");
+            list.forEach((txt, index) => {
+                if(txt.todo + txt.time === event.textContent && list[index].check !== true){
+                    list[index].check = true;
+                    localStorage.setItem("todo", JSON.stringify(list))
+                }else if (txt.todo === event.textContent && list[index].check === true){
+                    list[index].check = false;
+                    localStorage.setItem("todo", JSON.stringify(list))
+                }
+            });
         }
     })
     /////////////////////////////////////////    SAVE MASSAGE ONCLICK
@@ -143,20 +185,22 @@ window.addEventListener('DOMContentLoaded', function () {
         list.forEach(txt =>{
             let div = document.createElement('div'),
                 btn = document.createElement('div'),
-                str = document.createElement("p");
+                str = document.createElement("p"),
+                time = document.createElement('div');
             btn.classList.add('delete-massage');
             div.classList.add('massage');
             str.classList.add('massage-text');
+            time.classList.add('massage-time');
             str.textContent = txt.todo;
+            time.textContent = txt.time;
             massageArea.appendChild(div);
             div.appendChild(str);
+            div.appendChild(time);
             div.appendChild(btn);
             if (txt.check === true){
                 div.classList.toggle('done');
             }
         })
     }
-
-
 
 });
